ff288da5dec1b96d7e825013d1700b14d36daf5f
mahdinobar <nobar.mahdi@gmail.com>
2024-05-26 18:42:50 +0200

    simple subscriber to the grayscale image RO ZED wrapper, #2

--- mahdi/measurement_ros_2.py
+++ mahdi/measurement_ros_2.py
@@ -11,21 +11,22 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
 
-def callback(data):
-    # rospy.loginfo(rospy.get_caller_id() + '+++++++++++I heard %s', data.data)
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    print("cv_image=\n",cv_image)
-    print("np.asarray(cv_image)=\n",np.asarray(cv_image))
+class Server:
+    def __init__(self):
+        self.img_gray = None
+    def img_gray_callback(self, msg):
+        # "Store" message received.
+        # rospy.loginfo(rospy.get_caller_id() + '+++++++++++I heard %s', data.data)
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        print("timestamp={} [ns]".format(msg.header.stamp.nsecs))
+        # print("cv_image=\n",cv_image)
+        print("np.asarray(cv_image)=\n", np.asarray(cv_image))
 
-def listener():
-
-    rospy.init_node('listener')
-
-    rospy.Subscriber('/zedxm/zed_node/rgb/image_rect_gray', Image, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    rospy.init_node('listener')
+    server = Server()
+    rospy.Subscriber('/zedxm/zed_node/rgb/image_rect_gray', Image, server.img_gray_callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
